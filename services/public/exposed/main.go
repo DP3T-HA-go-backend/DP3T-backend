85497910d85960dc1ca3ae1da410aa57d493d2aa
David Carrera <dcarrera@ac.upc.edu>
2020-05-10 22:49:27 +0200

    POST is json-based

--- services/public/exposed/main.go
+++ services/public/exposed/main.go
@@ -6,6 +6,7 @@ import (
     "io"
     "io/ioutil"
     "net/http"
+    "encoding/json"
     "os"
 
     "github.com/julienschmidt/httprouter"
@@ -32,22 +33,17 @@ func exposed(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 func expose(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    exposee := &ProtoExposee{}
 
     in, err := ioutil.ReadAll(io.LimitReader(r.Body, 1024))
     if err != nil {
         log.Fatal("Error reading request:", err)
     }
 
-    if err := proto.Unmarshal(in, exposee); err != nil {
-        log.Fatal("Failed to parse Exposee: ", err)
-    }
-
-    data.Exposed = append(data.Exposed, exposee)
     fmt.Fprintln(os.Stdout, "POST: ", r.URL, ", body: ", string(in))
+    w.Header().Set("Content-Type", "application/json")
 
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprint(w, "OK\n")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode("Registered")
 }
 
 func hello(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -63,8 +59,8 @@ func main() {
 
     router := httprouter.New()
     router.GET("/", hello)
-    router.GET("/:date", exposed)
-    router.POST("/", expose)
+    router.GET("/exposed/:date", exposed)
+    router.POST("/exposed", expose)
 
     log.Fatal(http.ListenAndServe(":8080", router))
 }
