9e5ded7558eb4cd71045bd844f68784f7160b243
David Carrera <dcarrera@ac.upc.edu>
2020-05-11 08:41:04 +0200

    JWT token  using ECS256

--- services/public/exposed/main.go
+++ services/public/exposed/main.go
@@ -8,7 +8,14 @@ import (
     "net/http"
     "strconv"
     "time"
+    //"crypto/ecdsa"
+    "crypto/x509"
+    "encoding/pem"
+//    "crypto/elliptic"
+//    "crypto/rand"
+    //"crypto/sha256"
 
+    "github.com/dgrijalva/jwt-go"
     "github.com/julienschmidt/httprouter"
     "google.golang.org/protobuf/proto"
     "google.golang.org/protobuf/encoding/protojson"
@@ -16,15 +23,73 @@ import (
 
 var data ProtoExposedList
 
+
 func exposed(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     // TODO: Validate date & retrieve data based on it from key-value store
     // var date string
     // date = ps.ByName("date")
 
+    //type Token struct {
+    //  Raw       string                 // The raw token.  Populated when you Parse a token
+    //  Method    SigningMethod          // The signing method used or to be used
+    //  Header    map[string]interface{} // The first segment of the token
+    //  Claims    Claims                 // The second segment of the token
+    //  Signature string                 // The third segment of the token.  Populated when you Parse a token
+    //  Valid     bool                   // Is the token valid?  Populated when you Parse/Verify a token
+    //0
+    // https://godoc.org/github.com/dgrijalva/jwt-go#example-New--Hmac}
+
     w.Header().Set("Content-Type", "application/x-protobuf")
-    w.Header().Set("x-public-key", "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0RRZ0FFdkxXZHVFWThqcnA4aWNSNEpVSlJaU0JkOFh2UgphR2FLeUg2VlFnTXV2Zk1JcmxrNk92QmtKeHdhbUdNRnFWYW9zOW11di9rWGhZdjF1a1p1R2RjREJBPT0KLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg")
-    w.Header().Set("x-batch-release-time",strconv.FormatInt(makeTimestamp(),10))
+    w.Header().Set("x-public-key", "TUZrd0V3WUhLb1pJemowQ0FRWUlLb1pJemowREFRY0RRZ0FFWndxcUhIM1p6M05OZzd6aGtjZTIxbFpEdllyTWpEYW1BbG5TWXg2SHRBT0VMazlqR1VpeHRoeTk4MXpQc3N2bjdOYlFGVjdxcXFKMHdyWTJjR05pQ1E9PQ==")
+    w.Header().Set("x-batch-release-time",strconv.FormatInt(makeTimestampMillis(),10))
+
+    //mySigningKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+    //mySigningKey := []byte("MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgevZzL1gdAFr88hb2OF/2NxApJCzGCEDdfSp6VQO30hyhRANCAAQRWz+jn65BtOMvdyHKcvjBeBSDZH2r1RTwjmYSi9R/zpBnuQ4EiMnCqfMPWiZqB4QdbAd0E7oH50VpuZ1P087G")
+    //fmt.Println(mySigningKey, err)
+    mySigningKey, err0 := ioutil.ReadFile("/ec256-key")
+    if err0 != nil {
+		fmt.Println("Unable to load ECDSA private key: ", err0)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+    }
+    //fmt.Println("key: ",mySigningKey)
+    block, _ := pem.Decode(mySigningKey)
+    if block == nil || block.Type != "EC PRIVATE KEY" {
+		fmt.Println("failed to decode PEM block containing EC privatekey")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+    //fmt.Println("key: ",mySigningKey)
+    //var ecdsaKey *ecdsa.PrivateKey
+    //ecdsaKey, err2 := x509.ParsePKCS8PrivateKey(block.Bytes)
+    ecdsaKey, err2 := x509.ParseECPrivateKey(block.Bytes)
+    //
+    if err2 != nil {
+		fmt.Println("Unable to parse ECDSA private key: ", err2)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims {
+		"content-hash": "digesthereSHA256",
+		"hash-alg": "sha256",
+		"iss": "d3pt",
+		"iat": strconv.FormatInt(makeTimestampSeconds(),10),
+		"exp": strconv.FormatInt(makeTimestampSeconds()+1814400,10),
+		"batch-release-time": strconv.FormatInt(makeTimestampMillis(),10),
+             })
+    tokenString, err := token.SignedString(ecdsaKey)
+    fmt.Println(tokenString, err)
+    if err != nil {
+	// If there is an error in creating the JWT return an internal server error
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+    }
+
+    w.Header().Set("Signature", tokenString)
     w.WriteHeader(http.StatusOK)
+
 
     m, err := proto.Marshal(&data)
     if err != nil {
@@ -62,8 +127,12 @@ func hello(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
     fmt.Fprint(w, "Hello\n")
 }
 
-func makeTimestamp() int64 {
+func makeTimestampMillis() int64 {
     return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+func makeTimestampSeconds() int64 {
+    return time.Now().UnixNano() / int64(time.Second)
 }
 
 func main() {
