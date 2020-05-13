30a6b775a077b07986d726c67957421549af36a7
David Carrera <dcarrera@ac.upc.edu>
2020-05-13 11:01:07 +0200

    added authcode alpha to the src structure

--- src/go.mod
+++ src/go.mod
@@ -1,8 +1,12 @@
 module dp3t-backend
 
+go 1.14
+
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.4.1
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/spf13/viper v1.7.0
 	go.etcd.io/etcd v3.3.20+incompatible
 	google.golang.org/protobuf v1.22.0
 	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0
--- src/store/etcd.go
+++ src/store/etcd.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"dp3t-backend/api"
+	kvs "dp3t-backend/etcd"
+	"errors"
 
 	"crypto/tls"
 	"time"
@@ -10,9 +12,9 @@ import (
 )
 
 type Etcd struct {
-	Endpoints []string
-	TLSInfo transport.TLSInfo
-	DialTimeout time.Duration
+	Endpoints      []string
+	TLSInfo        transport.TLSInfo
+	DialTimeout    time.Duration
 	RequestTimeout time.Duration
 
 	TLS *tls.Config
@@ -22,7 +24,7 @@ func (e *Etcd) Init() error {
 	e.DialTimeout = 5 * time.Second
 	e.RequestTimeout = 10 * time.Second
 	e.Endpoints = []string{"10.0.26.10:2379", "10.0.26.11:2379", "10.0.26.13:2379"}
-	e.TLSInfo = transport.TLSInfo {
+	e.TLSInfo = transport.TLSInfo{
 		CertFile:      "/etc/ssl/etcd/ssl/node-node1.pem",
 		KeyFile:       "/etc/ssl/etcd/ssl/node-node1-key.pem",
 		TrustedCAFile: "/etc/ssl/etcd/ssl/ca.pem",
@@ -46,6 +48,10 @@ func (e *Etcd) AddExposee(exposee *api.ProtoExposee) error {
 }
 
 func (e *Etcd) AddAuthCode(code string) error {
+	r1 := kvs.KVPutIfNotExists(code, "")
+	if r1 != nil {
+		return errors.New("Authcode already existed")
+	}
 	return nil
 }
 
