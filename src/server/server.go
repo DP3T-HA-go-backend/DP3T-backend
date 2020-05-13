a3963594efd79323a4b18917e8893ab8a4012789
Jordà Polo <jorda.polo@bsc.es>
2020-05-13 19:20:58 +0200

    Configureable etcd

--- src/server/server.go
+++ src/server/server.go
@@ -24,6 +24,14 @@ type Config struct {
 	PrivateKeyFile string `ini:"private-key-file"`
 	StoreType      string `ini:"store"`
 	PrivateKey     *ecdsa.PrivateKey
+	EtcdConfig     *EtcdConfig
+}
+
+type EtcdConfig struct {
+	Endpoints  []string `ini:"endpoints" delim:","`
+	CertFile   string   `ini:"cert-file"`
+	KeyFile    string   `ini:"key-file"`
+	CAFile     string   `ini:"ca-file"`
 }
 
 func InitConfig(conf_file string) (*Config, error) {
@@ -55,6 +63,35 @@ func InitConfig(conf_file string) (*Config, error) {
 	conf.PrivateKey, e = x509.ParseECPrivateKey(block.Bytes)
 	if e != nil {
 		return conf, fmt.Errorf("Failed to parse EC private key: %s", e)
+	}
+
+	if conf.StoreType == "etcd" {
+		conf.EtcdConfig = &EtcdConfig{}
+
+		sec, e := i.GetSection("etcd")
+		if e != nil {
+			return conf, fmt.Errorf("Missing etcd section", e)
+		}
+
+		if e := sec.MapTo(conf.EtcdConfig); e != nil {
+			return conf, fmt.Errorf("Failed to decode etcd config: %s", e)
+		}
+
+		if len(conf.EtcdConfig.Endpoints) < 1 {
+			return conf, fmt.Errorf("No etcd endpoints")
+		}
+
+		if _, e := os.Stat(conf.EtcdConfig.CertFile); e != nil {
+			return conf, fmt.Errorf("Failed to read etcd cert file: %s", e)
+		}
+
+		if _, e := os.Stat(conf.EtcdConfig.KeyFile); e != nil {
+			return conf, fmt.Errorf("Failed to read etcd key file: %s", e)
+		}
+
+		if _, e := os.Stat(conf.EtcdConfig.CAFile); e != nil {
+			return conf, fmt.Errorf("Failed to read etcd CA file: %s", e)
+		}
 	}
 
 	return conf, nil
