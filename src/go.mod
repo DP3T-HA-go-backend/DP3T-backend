76d63810730662a148765f70c731d9aa37523ace
Jordà Polo <jorda.polo@bsc.es>
2020-05-13 09:21:10 +0200

    Add template for etcd integration

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module dp3t-backend
 require (
 	github.com/golang/protobuf v1.4.1
 	github.com/julienschmidt/httprouter v1.3.0
+	go.etcd.io/etcd v3.3.20+incompatible
 	google.golang.org/protobuf v1.22.0
 	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0
 	gopkg.in/ini.v1 v1.56.0
---
+++ src/store/etcd.go
@@ -0,0 +1,58 @@
+package store
+
+import (
+	"dp3t-backend/api"
+
+	"crypto/tls"
+	"time"
+
+	"go.etcd.io/etcd/pkg/transport"
+)
+
+type Etcd struct {
+	Endpoints []string
+	TLSInfo transport.TLSInfo
+	DialTimeout time.Duration
+	RequestTimeout time.Duration
+
+	TLS *tls.Config
+}
+
+func (e *Etcd) Init() error {
+	e.DialTimeout = 5 * time.Second
+	e.RequestTimeout = 10 * time.Second
+	e.Endpoints = []string{"10.0.26.10:2379", "10.0.26.11:2379", "10.0.26.13:2379"}
+	e.TLSInfo = transport.TLSInfo {
+		CertFile:      "/etc/ssl/etcd/ssl/node-node1.pem",
+		KeyFile:       "/etc/ssl/etcd/ssl/node-node1-key.pem",
+		TrustedCAFile: "/etc/ssl/etcd/ssl/ca.pem",
+	}
+
+	var err error
+	e.TLS, err = e.TLSInfo.ClientConfig()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *Etcd) GetExposed(timestamp int64) (*api.ProtoExposedList, error) {
+	return nil, nil
+}
+
+func (e *Etcd) AddExposee(exposee *api.ProtoExposee) error {
+	return nil
+}
+
+func (e *Etcd) AddAuthCode(code string) error {
+	return nil
+}
+
+func (e *Etcd) ExpireExposees() error {
+	return nil
+}
+
+func (e *Etcd) ExpireAuthCodes() error {
+	return nil
+}
